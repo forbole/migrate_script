6b8909c727361626d01868f37ce06f71149f35f7
huichiaotsou <huichiao.tsou@gmail.com>
2022-02-17 10:39:49 +0800

    change composite pk to unique keys

--- utils/create.js
+++ utils/create.js
@@ -25,9 +25,9 @@ async function NewTxTable() {
           logs         JSONB,
   
           /* Psql partition */
-          partition_id BIGINT NOT NULL,
-          PRIMARY KEY(hash, partition_id)
+          partition_id BIGINT NOT NULL
       )PARTITION BY LIST(partition_id);
+      ALTER TABLE transaction_new ADD UNIQUE (hash, partition_id);
       CREATE INDEX transaction_new_hash_index ON transaction_new (hash);
       CREATE INDEX transaction_new_height_index ON transaction_new (height);
       CREATE INDEX transaction_new_partition_id_index ON transaction_new (partition_id);
@@ -47,9 +47,9 @@ async function NewMsgTable() {
   
           /* Psql partition */
           partition_id                BIGINT NOT NULL,
-          height                      BIGINT NOT NULL,
-          PRIMARY KEY(transaction_hash, index, partition_id, height)
+          height                      BIGINT NOT NULL
       )PARTITION BY LIST(partition_id);
+      ALTER TABLE message_new ADD UNIQUE (transaction_hash, index, partition_id);
       CREATE INDEX message_new_transaction_hash_index ON message_new (transaction_hash);
       CREATE INDEX message_new_type_index ON message_new (type);
       CREATE INDEX message_new_involved_accounts_index ON message_new (involved_accounts_addresses);
