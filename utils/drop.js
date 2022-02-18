d300d52c558d4624c86c652138abd37d2022e3b9
Magic Cat <pusz.monika@gmail.com>
2022-02-18 12:42:40 +0000

    update output comments

--- utils/drop.js
+++ utils/drop.js
@@ -2,7 +2,7 @@
 const {query} = require("./psql")
 
 async function DropMessageByAddressFunc() {
-  console.log(`DROP FUNCTION IF EXISTS  messages_by_address(text[],text[],bigint,bigint);`)
+  console.log(`DROP FUNCTION IF EXISTS messages_by_address()`)
   await query(`DROP FUNCTION IF EXISTS messages_by_address(text[],text[],bigint,bigint);`)
 
 }
