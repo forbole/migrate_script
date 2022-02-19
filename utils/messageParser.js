e5f943f409162a9b6e8c7b00a2cd139176e04a08
huichiaotsou <huichiao.tsou@gmail.com>
2022-02-19 10:55:57 +0800

    add custom account parser

---
+++ utils/messageParser.js
@@ -0,0 +1,14 @@
+
+
+let customAccountParser = []
+
+let defaultAccountParser = [
+	"signer", "sender", "to_address", "from_address", "delegator_address",
+	"validator_address", "submitter", "proposer", "depositor", "voter",
+	"validator_dst_address", "validator_src_address",
+]
+
+module.exports = {
+    defaultAccountParser,
+    customAccountParser
+}
--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,5 @@
 const {query} = require("./psql")
+const {defaultAccountParser,customAccountParser}= require("./messageParser")
 
 async function selectFromOldTxTable(limit, offset) {
   const result = await query(`SELECT * FROM transaction_old ORDER BY height LIMIT ${limit} OFFSET ${offset}`)
@@ -6,27 +7,18 @@ async function selectFromOldTxTable(limit, offset) {
 }
   
 function messageParser(msg) {
+  const accountParsers = defaultAccountParser.concat(customAccountParser)
+  console.log(accountParsers.length);
+
   let involvedAddresses = "{"
-  
-  if(msg.signer != undefined) involvedAddresses += msg.signer + ","
-  if(msg.sender != undefined) involvedAddresses += msg.sender + ","
-  if(msg.to_address != undefined) involvedAddresses += msg.to_address + ","
-  if(msg.from_address != undefined) involvedAddresses += msg.from_address + ","
-  if(msg.delegator_address != undefined) involvedAddresses += msg.delegator_address + ","
-  if(msg.validator_address != undefined) involvedAddresses += msg.validator_address + ","
-  if(msg.submitter != undefined) involvedAddresses += msg.submitter + ","
-  if(msg.proposer != undefined) involvedAddresses += msg.proposer + ","
-  if(msg.depositor != undefined) involvedAddresses += msg.depositor + ","
-  if(msg.voter != undefined) involvedAddresses += msg.voter + ","
-  if(msg.validator_dst_address != undefined) involvedAddresses += msg.validator_dst_address + ","
-  if(msg.validator_src_address != undefined) involvedAddresses += msg.validator_src_address + ","
+  for(let role of accountParsers) if (msg[role] != undefined) involvedAddresses += msg[role] + ","
   if (msg.input != undefined) msg.input.forEach(i => {involvedAddresses += i.address  + ","})
   if (msg.output != undefined) msg.output.forEach(o => {involvedAddresses += o.address  + ","})
   
   involvedAddresses = involvedAddresses.slice(0 , -1) + "}"
   
   if (involvedAddresses.length == 1) {
-    // involvedAddresses == "{"
+    // when involvedAddresses == "{"
     return "{}"
   }
   
